0cef8d66df65343006cc66a96f458b6bd408c336
菅野　地紘 <k022g0111@g.neec.ac.jp>
2024-02-12 06:41:07 +0900

    6-2完成

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <list>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <thread>
+
+using namespace std;
+
+void Add(int num)
+{
+	num += 1;
+
+	printf("thread %d\n", num);
+};
+
+void Add2(int num)
+{
+	num += 2;
+
+	printf("thread %d\n", num);
+};
+
+void Add3(int num)
+{
+	num += 3;
+
+	printf("thread %d\n", num);
+};
+
+
+int main()
+{
+	int num = 0;
+
+	std::thread th1(Add, num);
+	std::thread th2(Add2, num);
+	std::thread th3(Add3, num);
+
+	th1.join();
+	th2.join();
+	th3.join();
+
+	return 0;
+}
